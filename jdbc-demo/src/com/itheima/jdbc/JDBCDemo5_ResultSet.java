a6c07c01a68ecb87c6c639a34c95d943269e119f
hyb2419805602 <2419805602@qq.com>
2022-11-04 23:30:15 +0800

    1234

--- jdbc-demo/src/com/itheima/jdbc/JDBCDemo5_ResultSet.java
+++ jdbc-demo/src/com/itheima/jdbc/JDBCDemo5_ResultSet.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 /**
@@ -24,5 +25,29 @@ public class JDBCDemo5_ResultSet {
         String password = "mysql";
         Connection conn =  DriverManager.getConnection(url, user, password);
 
+        // 3. 定义sql语句
+        String sql = "select * from account";
+
+        // 4. 获取statement对象
+        Statement stmt = conn.createStatement();
+
+        // 5. 执行sql语句
+        ResultSet rs = stmt.executeQuery(sql);
+
+        // 6. 处理结果, 遍历rs中的所有数据
+        // 6.1 光标向下移动一行, 并且判断当前行是否有数据.
+        while (rs.next()){
+            // 6.2 获取数据 getXxx();
+            int id = rs.getInt(1);
+            String name = rs.getString(2);
+            double money = rs.getDouble(3);
+
+            System.out.println("id:" + id + ", name:" + name + ", money:" + money);
+            System.out.println("===========");
+        }
+
+        // 7. 关闭资源
+        stmt.close();
+        conn.close();
     }
 }
