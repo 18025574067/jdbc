56a480529c0d073036c3f12484c6e7bd04a93235
hyb2419805602 <2419805602@qq.com>
2022-11-08 20:01:38 +0800

    1234

--- jdbc-demo/src/com/itheima/jdbc/JDBCDemo_7PreparedStatement.java
+++ jdbc-demo/src/com/itheima/jdbc/JDBCDemo_7PreparedStatement.java
@@ -23,6 +23,7 @@ public class JDBCDemo_7PreparedStatement {
 //        String pwd = "' or '1' = '1";
         String pwd = "123";
 
+        // 定义sql语句, 用?占位符代替.
         String sql = "select * from tb_user where username = ? and password = ?";
 
         // 获取pstmt对象
@@ -32,7 +33,7 @@ public class JDBCDemo_7PreparedStatement {
         pstmt.setString(1, name);
         pstmt.setString(2, pwd);
 
-        // 执行sql语句
+        // 执行sql语句, 不需要再传入sql了.
         ResultSet rs = pstmt.executeQuery();
 
         // 判断登录是否成功
@@ -50,14 +51,14 @@ public class JDBCDemo_7PreparedStatement {
     }
 
     /**
-     SQL防注入.
+     PreparedStatement原理.
      */
     @Test
     public void testPreparedStatement2() throws Exception {
         // 2. 获取连接 如果连接是本地的MySQL, 并且端口是3306, 可以简化书写.
         // useServerPrepStmts=true 参数开户预编译功能。
 //        String url = "jdbc:mysql:///db1?useSSL=false";
-        String url = "jdbc:mysql:///db1?useSSL=false&useServerPrepStmts=true";
+        String url = "jdbc:mysql:///db1?useSSL=false&useServerPrepStems=true";
         String user = "root";
         String password = "mysql";
         Connection conn =  DriverManager.getConnection(url, user, password);
