51d6614bdd460e1c4f8f93939a75adfc8f0603b5
hyb2419805602 <2419805602@qq.com>
2022-11-08 21:14:58 +0800

    1234

--- jdbc-demo/src/com/itheima/jdbc/JDBCDemo7_PreparedStatement.java
+++ jdbc-demo/src/com/itheima/jdbc/JDBCDemo7_PreparedStatement.java
@@ -65,8 +65,8 @@ public class JDBCDemo7_PreparedStatement {
 
         // 接收用户输入的用户名和密码
         String name = "张三";
-        String pwd = "' or '1' = '1";
-//        String pwd = "123";
+//        String pwd = "' or '1' = '1";
+        String pwd = "123";
 
         String sql = "select * from tb_user where username = ? and password = ?";
 
@@ -75,12 +75,21 @@ public class JDBCDemo7_PreparedStatement {
 
         Thread.sleep(5000);
 
+        ResultSet rs = null;
+
         // 设置?的值
         pstmt.setString(1, name);
         pstmt.setString(2, pwd);
 
         // 执行sql语句
-        ResultSet rs = pstmt.executeQuery();
+        rs = pstmt.executeQuery();
+
+        // 设置?的值
+        pstmt.setString(1, "aaa");
+        pstmt.setString(2, "bbb");
+
+        // 执行sql语句
+        rs = pstmt.executeQuery();
 
         // 判断登录是否成功
         if (rs.next()){
