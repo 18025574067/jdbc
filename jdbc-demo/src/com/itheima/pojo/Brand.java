3c84ef83c934c4b29ca086c0d6b8dfc1708b4be3
AA <2419805602@qq.com>
2022-11-11 22:10:42 +0800

    122

--- jdbc-demo/src/com/itheima/pojo/Brand.java
+++ jdbc-demo/src/com/itheima/pojo/Brand.java
@@ -2,6 +2,27 @@ package com.itheima.pojo;
 
 /**
  * 品牌
+ * alt + 鼠标左键 ：整列编辑。
  */
 public class Brand {
+    
+    // id 主键
+    private int id;
+	// 品牌名称
+    private String brandName;
+	// 企业名称
+    private String companyName;
+	// 排序字段
+    private int ordered;
+    // 描述信息
+    private String description;
+	// 状态： 0：禁用，1：启用
+//    private int status = 0;
+    private Integer status;
+    
+    
+    
+    
+    
+    
 }
