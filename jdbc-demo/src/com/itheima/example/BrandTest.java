9936f215f31a0403e562071c31a12b4912fc49c2
hyb2419805602 <2419805602@qq.com>
2022-11-12 22:54:52 +0800

    1234

--- jdbc-demo/src/com/itheima/example/BrandTest.java
+++ jdbc-demo/src/com/itheima/example/BrandTest.java
@@ -127,4 +127,67 @@ public class BrandTest {
         pstms.close();
         conn.close();
     }
+
+    @Test
+    public void testUpdate() throws Exception {
+        // 接收页面提交的数据
+        String brandName = "香飘飘";
+        String companyName = "香飘飘股份有限公司";
+        int ordered = 1000;
+        String description = "绕地球一圈";
+        int status = 1;
+        int id = 4;
+
+        /**
+         * 修改
+         * 1. SQL:
+             update tb_brand
+                set brand_name=?,
+                company_name=?,
+                ordered=?,
+                description=?,
+                status=?
+             where id=?;
+         * 2. 参数: 需要所有参数.
+         * 3. 结果: 返回值为boolean.
+         */
+        // 1. 获取Connection对象
+        // 3. 加载配置文件
+        Properties prop = new Properties();
+        prop.load(new FileInputStream("../jdbc-demo/src/druid.properties"));
+        // 4. 获取连接池对象
+        DataSource dataSource = DruidDataSourceFactory.createDataSource(prop);
+        // 5. 获取数据库连接 connection
+        Connection conn = dataSource.getConnection();
+
+        // 2. 定义sql语句
+        String sql = " update tb_brand \n" +
+                "                set brand_name=?, \n" +
+                "                company_name=?, \n" +
+                "                ordered=?, \n" +
+                "                description=?, \n" +
+                "                status=? \n" +
+                "             where id=?;";
+
+        // 3. 获取pstms对象
+        PreparedStatement pstms = conn.prepareStatement(sql);
+
+        // 4. 设置参数
+        pstms.setString(1, brandName);
+        pstms.setString(2, companyName);
+        pstms.setInt(3, ordered);
+        pstms.setString(4, description);
+        pstms.setInt(5, status);
+        pstms.setInt(6, id);
+
+        // 5. 执行sql语句
+        int count = pstms.executeUpdate(); // 影响的行数.
+
+        // 6. 输出结果
+        System.out.println(count > 0);
+
+        // 7. 释放资源
+        pstms.close();
+        conn.close();
+    }
 }
