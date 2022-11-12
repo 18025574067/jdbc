4169d45e9a44108e084e9dd8b6a7336070000ca1
hyb2419805602 <2419805602@qq.com>
2022-11-12 22:26:25 +0800

    1234

--- jdbc-demo/src/com/itheima/example/BrandTest.java
+++ jdbc-demo/src/com/itheima/example/BrandTest.java
@@ -1,6 +1,7 @@
 package com.itheima.example;
 
 import com.alibaba.druid.pool.DruidDataSourceFactory;
+import com.itheima.pojo.Brand;
 import org.junit.Test;
 
 import javax.sql.DataSource;
@@ -8,6 +9,8 @@ import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -24,18 +27,17 @@ public class BrandTest {
          * 2. 参数: 不需要
          * 3. 结果: List<Brand>
          */
-
         // 1. 获取Connection对象
         // 3. 加载配置文件
         Properties prop = new Properties();
-        prop.load(new FileInputStream("jdbc-demo/src/druid.properties"));
+        prop.load(new FileInputStream("../jdbc-demo/src/druid.properties"));
         // 4. 获取连接池对象
         DataSource dataSource = DruidDataSourceFactory.createDataSource(prop);
         // 5. 获取数据库连接 connection
         Connection conn = dataSource.getConnection();
 
         // 2. 定义sql语句
-        String sql = "select * from tb_brand";
+        String sql = "select * from tb_brand;";
 
         // 3. 获取pstms对象
         PreparedStatement pstms = conn.prepareStatement(sql);
@@ -46,17 +48,82 @@ public class BrandTest {
         ResultSet rs = pstms.executeQuery();
 
         // 6. 处理结果, List<Brand> 封装Brand对象, 装载List对象
+        Brand brand = null;
+        List<Brand> brands = new ArrayList<>();
         while (rs.next()){
             // 获取数据
+            int id = rs.getInt("id");
+            String brandName = rs.getString("brand_name");
+            String companyName = rs.getString("company_name");
+            int ordered = rs.getInt("ordered");
+            String description = rs.getString("description");
+            int status = rs.getInt("status");
 
             // 封装Brand对象
+            brand = new Brand();
+            brand.setId(id);
+            brand.setBrandName(brandName);
+            brand.setCompanyName(companyName);
+            brand.setOrdered(ordered);
+            brand.setDescription(description);
+            brand.setStatus(status);
 
             // 装载集合
-
+            brands.add(brand);
         }
+
+        System.out.println(brands);
 
         // 7. 释放资源
         rs.close();
+        pstms.close();
+        conn.close();
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+        // 接收页面提交的数据
+        String brandName = "香飘飘奶茶";
+        String companyName = "香飘飘股份有限公司";
+        int ordered = 1;
+        String description = "绕地球一圈";
+        int status = 1;
+
+        /**
+         * 添加
+         * 1. SQL:insert into tb_brand(brand_name, company_name, ordered, description, status) values(?, ?, ?, ?, ?);
+         * 2. 参数: 除了id之外所有参数.
+         * 3. 结果: List<Brand>
+         */
+        // 1. 获取Connection对象
+        // 3. 加载配置文件
+        Properties prop = new Properties();
+        prop.load(new FileInputStream("../jdbc-demo/src/druid.properties"));
+        // 4. 获取连接池对象
+        DataSource dataSource = DruidDataSourceFactory.createDataSource(prop);
+        // 5. 获取数据库连接 connection
+        Connection conn = dataSource.getConnection();
+
+        // 2. 定义sql语句
+        String sql = "insert into tb_brand(brand_name, company_name, ordered, description, status) values(?, ?, ?, ?, ?);";
+
+        // 3. 获取pstms对象
+        PreparedStatement pstms = conn.prepareStatement(sql);
+
+        // 4. 设置参数
+        pstms.setString(1, brandName);
+        pstms.setString(2, companyName);
+        pstms.setInt(3, ordered);
+        pstms.setString(4, description);
+        pstms.setInt(5, status);
+
+        // 5. 执行sql语句
+        int count = pstms.executeUpdate(); // 影响的行数.
+
+        // 6. 输出结果
+        System.out.println(count > 0);
+
+        // 7. 释放资源
         pstms.close();
         conn.close();
     }
