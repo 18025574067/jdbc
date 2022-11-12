397da6e4d98328dfcc725913763166defb4663c3
hyb <2419805602@qq.com>
2022-11-12 18:18:33 +0800

    12345

---
+++ jdbc-demo/src/com/itheima/example/BrandTest.java
@@ -0,0 +1,19 @@
+package com.itheima.example;
+
+import org.junit.Test;
+
+/**
+ *
+ * 品牌数据的增删改查操作
+ *
+ */
+public class BrandTest {
+    @Test
+    public void testSelectAll(){
+
+
+    }
+
+
+
+}
